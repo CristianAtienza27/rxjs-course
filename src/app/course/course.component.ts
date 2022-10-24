3c95db4c38c2708e71bc8f9fe82e3c28ebae07e0
CristianAtienza27 <cristianatienza26@gmail.com>
2022-10-24 11:25:06 +0200

    The RxJs Debug Operator - Implementation Conclusion

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -11,11 +11,12 @@ import {
     concatMap,
     switchMap,
     withLatestFrom,
-    concatAll, shareReplay, throttle
+    concatAll, shareReplay
 } from 'rxjs/operators';
-import { merge, fromEvent, Observable, concat, interval } from 'rxjs';
+import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
+import { debug, RxJsLogginLevel, setRxJsLogginLevel } from '../common/debug';
 
 
 @Component({
@@ -42,18 +43,27 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         this.courseId = this.route.snapshot.params['id'];
 
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+            .pipe(
+                debug( RxJsLogginLevel.INFO, 'course value ')
+            );
+
+        setRxJsLogginLevel(RxJsLogginLevel.DEBUG);
 
     }
 
     ngAfterViewInit() {
 
-        fromEvent<any>(this.input.nativeElement, 'keyup')
+        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
             .pipe(
                 map(event => event.target.value),
-                throttle(() => interval(500)),
-            )
-            .subscribe(console.log);
+                startWith(''),
+                debug(RxJsLogginLevel.TRACE, 'search '),
+                debounceTime(400),
+                distinctUntilChanged(),
+                switchMap(search => this.loadLessons(search)),
+                debug( RxJsLogginLevel.DEBUG, 'lessons value ')
+            );
     }
 
     loadLessons(search = ''): Observable<Lesson[]>{
