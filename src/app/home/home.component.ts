c2c3de47942d0d4075d9c4d89f02731a4339f1f0
CristianAtienza27 <cristianatienza26@gmail.com>
2022-10-24 13:18:29 +0200

    Store Service Design - What Subject to Use?

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,10 @@ export class HomeComponent implements OnInit {
 
     advancedCourses$: Observable<Course[]>;
 
+    constructor(){
+        
+    }
+
     ngOnInit() {
 
         const http$ = createHttpObservable('/api/courses');
